3d78bf4c58ce3cb6fd7b13f1097e1e24bf877ffc
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-11-03 20:44:28 +0200

    Create J - Average 2.cpp

---
+++ ICPC Mansoura/Level 0/J - Average 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#define itn int
+#define fro for
+#include <string>
+#include <cstdlib>
+#define ll long long
+using namespace std;
+
+int main(){
+double a,b,c;
+cin>>a;
+cin>>b;
+cin>>c;
+
+cout<<"MEDIA = "<<fixed<<setprecision(1)<<(a*2+b*3+c*5)/(2+5+3)<<endl;
+
+}
+
