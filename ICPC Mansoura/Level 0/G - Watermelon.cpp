b4d82917ff6bfb236661114dce588410503ada38
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-11-03 20:14:38 +0200

    Create G - Watermelon.cpp

---
+++ ICPC Mansoura/Level 0/G - Watermelon.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#define itn int
+#define fro for
+#include <string>
+#include <cstdlib>
+#define ll long long
+using namespace std;
+
+int main(){
+int n;
+cin>>n;
+
+if(n%2==0&&n!=0&&n!=2){
+    cout<<"YES"<<endl;
+}else cout<<"NO"<<endl;
+}
+
