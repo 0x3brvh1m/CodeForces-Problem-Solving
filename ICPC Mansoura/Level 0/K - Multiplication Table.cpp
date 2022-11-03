14c179a9fd874d39b593be8cf8f54812a9243d56
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-11-03 20:47:35 +0200

    Create K - Multiplication Table.cpp

---
+++ ICPC Mansoura/Level 0/K - Multiplication Table.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#define itn int
+#define fro for
+#include <string>
+#include <cstdlib>
+#define ll long long
+using namespace std;
+
+int main(){
+int n ;
+cin>>n;
+for(int i = 1 ; i<=10 ; i++){
+    cout<<i<<" x "<<n<<" = "<<i*n<<endl;
+}
+}
+
