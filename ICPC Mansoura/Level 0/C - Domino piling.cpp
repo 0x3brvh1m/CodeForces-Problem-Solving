e6e8f8881cd049787e302dc5a511df8310475a6f
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-11-03 20:00:52 +0200

    Create C - Domino piling.cpp

---
+++ ICPC Mansoura/Level 0/C - Domino piling.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#define itn int
+#define fro for
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+
+int main(){
+int a,b;
+cin>>a>>b;
+cout<<(a*b)/2<<endl;
+}
+
