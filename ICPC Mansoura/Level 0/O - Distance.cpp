4450cafaf588f362529a299ba7361f946d66e9ea
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-11-03 21:52:00 +0200

    Create O - Distance.cpp

---
+++ ICPC Mansoura/Level 0/O - Distance.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#define itn int
+#define fro for
+#include <string>
+#include <cstdlib>
+#define ll long long
+using namespace std;
+
+int main(){
+ll n;
+cin>>n;
+cout<<n*2<<" minutos"<<endl;
+}
+
