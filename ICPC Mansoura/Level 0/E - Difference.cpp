f02b292a5b91046d2d9f63b7e8bab6151d301121
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-11-03 20:04:55 +0200

    Create E - Difference.cpp

---
+++ ICPC Mansoura/Level 0/E - Difference.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#define itn int
+#define fro for
+#include <string>
+#include <cstdlib>
+#define ll long long
+using namespace std;
+
+int main(){
+ll a,b,c,d;
+cin>>a;
+cin>>b;
+cin>>c;
+cin>>d;
+cout<<"DIFERENCA = "<<(a*b)-(c*d)<<endl;
+}
