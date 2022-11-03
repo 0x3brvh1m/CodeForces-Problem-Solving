507a7f17153b431bec11ac97ae0f1254e9d79cc9
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-11-03 19:59:53 +0200

    Create A - Hello World!.cpp

---
+++ ICPC Mansoura/Level 0/A - Hello World!.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#define itn int
+#define fro for
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+  cout<<"Hello World!"<<endl;
+return 0;
+}
