8f107a68b8313a90ef67eb6f0ea9c92627e907a8
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-11-03 20:01:15 +0200

    Create D - Area of a Circle.cpp

---
+++ ICPC Mansoura/Level 0/D - Area of a Circle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <math.h>
+#include <algorithm>
+#define itn int
+#define fro for
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+const double PI = 3.14159;
+double r;
+cin>>r;
+cout<<"A="<<fixed<<setprecision(4)<<PI*r*r<<endl;
+}
