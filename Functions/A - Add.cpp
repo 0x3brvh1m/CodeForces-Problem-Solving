468012d61458e5f8a5d3c1311a41be2f94b64f0b
Cyber Anubis <107937836+x86-Anubis@users.noreply.github.com>
2022-12-17 17:09:29 +0200

    Create A - Add.cpp

---
+++ Functions/A - Add.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <math.h>
+#include <cmath>
+#include <deque>
+#include <iterator>
+#include <numeric>
+#include <bits/stdc++.h>
+#include <string>
+#define itn int
+#include <regex>
+#define ll long long
+#define cn(v) for(auto &(i):v)cin>>(i)
+#define co(v) for(auto i:v)cout<<(i)
+using namespace std;
+
+void add(ll x, ll y){
+    cout<<x+y;
+}
+int main() {string s;
+    ll x,y;
+    cin>>x>>y;
+    add(x,y);
+    return 0;
+}
+
+
+
+
